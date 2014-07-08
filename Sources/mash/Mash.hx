4bbff0883e6bf85410695e29e369a3da3fb4dc12
Rahazan <guido.reaver@gmail.com>
2014-07-08 02:13:38 +0200

    Created HIDE project

--- Sources/mash/Mash.hx
+++ Sources/mash/Mash.hx
@@ -1,9 +1,27 @@
 package mash;
 
 import kha.Game;
+import kha.Painter;
+import kha.Color;
 
 class Mash extends Game {
 	public function new() {
 		super("Mash", false);
+		
+		
 	}
+	
+	override public function update() : Void {
+		
+	}
+	
+	override public function render(painter: Painter): Void {
+		startRender(painter);
+		
+		painter.setColor(Color.fromBytes(255, 255, 0));
+		painter.fillRect(0, 0, width, height);
+		
+		endRender(painter);
+	}
+	
 }
