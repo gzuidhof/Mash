c41bc8fd81265cc3d479b97e8db757b148a28e04
Rahazan <guido.reaver@gmail.com>
2014-07-17 01:33:07 +0200

    Moved back Kha, necessary for khamake

---
+++ src/mash/serialization/SceneSerializer.hx
@@ -0,0 +1,9 @@
+package mash.serialization;
+
+import mash.core.MashScene;
+
+interface SceneSerializer
+{
+	public function serializeScene(scene : MashScene): String;
+	public function deSerializeScene(text : String): String;
+}
