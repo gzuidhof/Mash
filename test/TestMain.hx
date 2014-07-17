12980605d44fef9158cd73d71d06f3d03fd38716
Rahazan <guido.reaver@gmail.com>
2014-07-17 04:12:59 +0200

    Removed reference to removed tests

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -4,9 +4,8 @@ class TestMain {
     
     static function main(){
         var r = new haxe.unit.TestRunner();
-        r.add(new TestExample());
+        
 		
-
         // Run the tests
         r.run();
 		
