6dcf34f13ffbe67e24c000a6847a80b2165afcfc
Rahazan <guido.reaver@gmail.com>
2014-08-21 07:02:44 +0200

    Refactored MashLoadingScreen

--- src/mash/core/MashLoadingScreen.hx
+++ src/mash/core/MashLoadingScreen.hx
@@ -30,31 +30,45 @@ class MashLoadingScreen extends Game
 	{
 		this.logo = Loader.the.getImage("mashlogo");
 		this.logoLight = Loader.the.getImage("mashlogo-light");
+		
+		//Do actual loading here.
 	}
 
 	
 	override public function render(painter: kha.Painter): Void 
 	{
 		startRender(painter);
+		
 		//Draw background
 		painter.setColor(kha.Color.fromBytes(240, 240, 240));
 		painter.fillRect(0, 0, width, height);
 		
 		if (logo != null)
 		{
-			//Draw logo backdrop
-			painter.drawImage(logoLight, width / 2 - logoLight.width / 2, height / 2 - logoLight.height / 2);
-			
-			//Draw logo, transparancy doesn't seem to be working yet.
-			painter.setColor(kha.Color.fromFloats(1.0, 1.0, 1.0, Loader.the.getLoadPercentage()/100));
-			painter.drawImage(logo, width / 2 - logo.width/2, height / 2 - logo.height/2);
+			renderLogo();
 		}
 		
-		//Draw loading bar
-		painter.setColor(kha.Color.fromBytes(255, 255, 255, 255));
-		painter.fillRect(0, height / 2 + 80 , Loader.the.getLoadPercentage() * width / 100, 20);
-		
+		drawLoadingBar();
+=
 		endRender(painter);
 	}
+	
+	function renderLogo():Void 
+	{
+		//Draw logo backdrop
+		painter.drawImage(logoLight, width / 2 - logoLight.width / 2, height / 2 - logoLight.height / 2);
+		
+		//Draw logo, transparancy doesn't seem to be working yet.
+		painter.setColor(kha.Color.fromFloats(1.0, 1.0, 1.0, Loader.the.getLoadPercentage()/100));
+		painter.drawImage(logo, width / 2 - logo.width/2, height / 2 - logo.height/2);
+	}
+	
+	function drawLoadingBar():Void 
+	{
+		painter.setColor(kha.Color.fromBytes(255, 255, 255, 255));
+		painter.fillRect(0, height / 2 + 80 , Loader.the.getLoadPercentage() * width / 100, 20);
+	}
+	
+	
 	
 }
