58a5086b9426dba5ea891346655be94b9ea723a4
Rahazan <guido.reaver@gmail.com>
2014-08-21 06:26:52 +0200

    Added Loading Screen, removed old test stuff

--- src/mash/Mash.hx
+++ src/mash/Mash.hx
@@ -1,11 +1,13 @@
 package mash;
 
 import mash.core.MashEngine;
+import mash.core.MashLoadingScreen;
 
 class Mash {
 	public function new() {
 		var starter = new kha.Starter();
-		starter.start(new MashEngine());
+		//starter.start(new MashEngine());
+		starter.start(new MashLoadingScreen("Mash", ""));
 	}
 }
 
---
+++ src/mash/core/MashLoadingScreen.hx
@@ -0,0 +1,62 @@
+package mash.core;
+
+import kha.Image;
+import kha.Game;
+import kha.Loader;
+
+/**
+ * Default loading screen for a Mash game.
+ * @author Guido
+ */
+class MashLoadingScreen extends Game
+{
+	private var gameName: String;
+	private var sceneToLoad: String;
+	
+	private var logo: Image;
+	private var logoLight: Image;
+	
+	public function new(gameName:String, sceneToLoad:String) 
+	{
+		super(gameName, false);
+	}
+	
+	override public function init(): Void 
+	{
+		Loader.the.loadRoom("mashloadingscreen", startLoader);
+	}
+	
+	public function startLoader(): Void
+	{
+		this.logo = Loader.the.getImage("mashlogo");
+		this.logoLight = Loader.the.getImage("mashlogo-light");
+	}
+	
+	private var i = 0;
+	
+	override public function render(painter: kha.Painter): Void 
+	{
+		startRender(painter);
+		i++;
+		//Draw background
+		painter.setColor(kha.Color.fromBytes(240, 240, 240));
+		painter.fillRect(0, 0, width, height);
+		
+		if (logo != null)
+		{
+			//Draw logo backdrop
+			painter.drawImage(logoLight, width / 2 - logoLight.width / 2, height / 2 - logoLight.height / 2);
+			
+			//Draw logo, transparancy doesn't seem to be working yet.
+			painter.setColor(kha.Color.fromFloats(1.0, 1.0, 1.0, Loader.the.getLoadPercentage()/100));
+			painter.drawImage(logo, width / 2 - logo.width/2, height / 2 - logo.height/2);
+		}
+		
+		//Draw loading bar
+		painter.setColor(kha.Color.fromBytes(255, 255, 255, 255));
+		painter.fillRect(0, height / 2 + 80 , Loader.the.getLoadPercentage() * width / 100, 20);
+		
+		endRender(painter);
+	}
+	
+}
