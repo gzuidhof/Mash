7f9d42be08feb402c6063ecc3081cb59edca3a0a
Rahazan <guido.reaver@gmail.com>
2014-09-17 04:06:17 +0200

    Removed Kha

--- src/mash/Mash.hx
+++ src/mash/Mash.hx
@@ -1,13 +1,10 @@
 package mash;
 
 import mash.MashEngine;
-import mash.MashLoadingScreen;
 
 class Mash {
 	public function new() {
-		var starter = new kha.Starter();
-		//starter.start(new MashEngine());
-		starter.start(new MashLoadingScreen("scene1"));
+		new MashEngine();
 	}
 }
 
--- src/mash/MashEngine.hx
+++ src/mash/MashEngine.hx
@@ -3,22 +3,15 @@ package mash;
 import ash.core.Engine;
 import ash.core.Entity;
 import haxe.Int64;
-import kha.Framebuffer;
-import kha.Image;
-import kha.Game;
-import kha.Color;
-import kha.Sys;
-import kha.Loader;
+import haxe.Timer;
 import mash.component.ImageDisplay;
 import mash.component.Position;
 import mash.system.ImageRenderSystem;
 
-class MashEngine extends Game
+class MashEngine
 {
 	
 	public var mashScene : MashScene;
-	public var backbuffer: Image;
-	public var graphics2: kha.graphics2.Graphics;
 	
 	private var initialized = false;
 	
@@ -40,55 +33,46 @@ class MashEngine extends Game
 	
 	public function new() 
 	{
-		super(Loader.the.name, false);
-		
 		mashScene = new MashScene("_newScene");
 	}	
 	
-	
-	override public function init(): Void 
+	public function init(): Void 
 	{ 
-		backbuffer = Image.createRenderTarget(width, height);
-		graphics2 = backbuffer.g2;
-		lastFrameTime = timeSinceStart = Sys.getTime();
+		lastFrameTime = timeSinceStart = getTime();
 		initialized = true;
-	}
-	
-	override public function update(): Void 
-	{
-		
 	}
 	
 	public function loadScene(sceneName: String, callback: Void->Void) 
 	{
-		Loader.the.loadRoom(sceneName, callback);
+		//Loader.the.loadRoom(sceneName, callback);
 	}
 	
 	public function startScene(sceneName: String)
 	{
+	/*
 		if (Loader.the.isBlobAvailable(sceneName + ".hxscene"))
 		{
 			var text = Loader.the.getBlob(sceneName + ".hxscene").toString();
 			var sceneSerializer = new mash.serialization.scene.HaxeStdSceneSerializer();
 			mashScene = sceneSerializer.deSerializeScene(text);
 		}
+	*/
 	}
 	
 	
-	override public function render(frame: kha.Framebuffer): Void
+	public function update(): Void
 	{
 		if (!initialized) return;
 		
-		deltaTime = Sys.getTime() - lastFrameTime;
-		lastFrameTime = Sys.getTime();
+		deltaTime = getTime() - lastFrameTime;
+		lastFrameTime = getTime();
 		
-		graphics2.begin();
 		mashScene.update(deltaTime);
-		graphics2.end();
 		
-		startRender(frame);
-		kha.Scaler.scale(backbuffer, frame, kha.Sys.screenRotation);
-		endRender(frame);
-		
+	}
+	
+	public static function getTime(): Float {
+		return Timer.stamp();
+		//return flash.Lib.getTimer() / 1000.0;
 	}
 }
